687021ff374d14b150666b0bc7b5e1f6d78680bd
ugikMat <ugik.mats@gmail.com>
2017-12-24 21:42:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mat/fixerkurs/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mat.fixerkurs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+}
